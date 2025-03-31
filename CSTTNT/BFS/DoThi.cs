fc444ae264e55b2e1140abb31f7b138a67450592
Your Name <you@example.com>
2025-03-31 14:14:21 +0700

    ok

---
+++ CSTTNT/BFS/DoThi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    class DoThi
+    {
+        public int n;
+        public int start;
+        public int goal;
+        public int[,] arr;
+        
+       public void readFile(String path)
+        {
+
+        }
+    }
+}
